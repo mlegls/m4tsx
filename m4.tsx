4c9cef1a5762b94b3458db1aa9fd2e017d11c5da
疒奀 <46226846+mlegls@users.noreply.github.com>
2023-01-18 06:06:18 -0800

    basics

---
+++ m4.tsx
@@ -0,0 +1,14 @@
+//divert(`-1')
+//@ts-nocheck
+/*
+include(`capitalize.m4')
+define(`XC',`const [$1`Change$, set'_capitalize($1)`] = createSignal(), [use'_capitalize($1)`, '$1`$] = bind('$1`Change$, '$2)')
+define(`RUS',`const [$1`, set'_capitalize($1)] = useState($2)')
+define(`RUX',`const $1` = use'_capitalize($1)()')
+define(`RR',`return (<div>$1</div>)')
+define(`RC',`<$1 $2>$3</$1>')
+define(`PC',`className="$1"')
+define(`PO',`on$1={($2)=>$3}')
+define(`POC',PO(Click,$1,$2))
+divert`'dnl
+*/
